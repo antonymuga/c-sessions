55f7381f0e2ac10305bc44a3a31935e37668068a
antonymuga <anthony.ochieng.muga@gmail.com>
2023-03-09 00:24:25 -0800

    Recompiled arg.c

--- arg.c
+++ arg.c
@@ -3,6 +3,15 @@
 
 int main(int argc, char* argv[])
 {
+	int index, sum;
 	printf("argc = %d\n", argc);
+	printf ("Let's see what is in argv[]\n");
+	for (index = 1; index < argc; index++)
+	{
+		printf("argv[%d] = %s\n", index, argv[index]);
+		sum = sum + atoi(argv[index]);
+	}
+	printf("Total = %d\n", sum);
+
 	return (0);
 }
